170558c402f0a89242295809aa93255755665369
Brayanene <merinobrayan831@gmail.com>
2026-04-16 19:30:37 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function mover() {
+    document.getElementById("t").style.transform = "translate(80px)";
+}
+
+function escalar() {
+    document.getElementById("e").style.transform = "scale(1.6)";
+}
+
+function rotar() {
+    document.getElementById("r").style.transform = "rotate(90deg)";
+}
+
+function sesgar() {
+    document.getElementById("s").style.transform = "skew(30deg)";
+}
